fdda70137a5ab8dfa2c3e2ba7c4744a01121b4d0
Christopher <xtopher.brandt@gmail.com>
2018-06-14 16:45:07 -0700

    Shift code around

---
+++ googleButtonFormat.js
@@ -0,0 +1,12 @@
+
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSignIn,
+      'onfailure': onFailure
+    });
+  }
--- main.js
+++ main.js
@@ -20,15 +20,3 @@ function signOut() {
     $("#userId").attr( "hidden", "" );
     $("#signin").removeAttr( "hidden" );
 }
-
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSignIn,
-      'onfailure': onFailure
-    });
-  }
